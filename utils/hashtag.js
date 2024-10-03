7d8fff3fce8dc416e83e69c64938ab0884729317
Junghwa <jungh150@naver.com>
2024-10-03 11:47:56 +0900

    해시태그 관련 함수를 관리할 파일 hashtag.js 생성

---
+++ utils/hashtag.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import asyncHandler from '../utils/asyncHandler.js';
+
+const prisma = new PrismaClient();
+export async function addHashtag() {
+
+}
